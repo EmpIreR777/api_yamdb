cd48b61ac60e3015cd9f130949f06e8dfd474583
Fedor Samofalov <smofalovfd@gmail.com>
2024-07-16 11:42:04 +0300

    Описаны модели категорий, жанров и произведений.

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,67 @@
+from datetime import datetime
+
+from django.core.validators import MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class BaseCategoryGenreModel(models.Model):
+    """Базовая модель для категорий и жанров."""
+    name = models.CharField('Название', max_length=256)
+    slug = models.SlugField('Слаг', max_length=50, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.slug
+
+
+class Category(BaseCategoryGenreModel):
+    """Модель категорий произведения."""
+
+    class Meta:
+        vebrose_name = 'Категория'
+        vebrose_name_plural = 'Категории'
+
+
+class Genre(BaseCategoryGenreModel):
+    """Модель жанров произведения."""
+
+    class Meta:
+        vebrose_name = 'Жанр'
+        vebrose_name_plural = 'Жанры'
+
+
+class Title(models.Model):
+    """Модель произведения."""
+    name = models.CharField('Название', max_length=256)
+    year = models.IntegerField(
+        'Год выпуска',
+        validators=[
+            MaxValueValidator(
+                datetime.now().year,
+                'Год выпуска не может быть больше текущего'
+            )
+        ]
+    )
+    description = models.TextField('Описание', null=True, blank=True)
+    genre = models.ManyToManyField(
+        Genre,
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        verbose_name='Жанр'
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        verbose_name='Категория'
+    )
+
+    class Meta:
+        vebrose_name = 'Произведение'
+        vebrose_name_plural = 'Произведения'
+        ordering = ('year',)
+
+    def __str__(self):
+        return self.name
