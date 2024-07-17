85087acde66b3686a7d1b79584a568bdb5544983
Kirill Safronov <Kirill.tanatos1993@mail.ru>
2024-07-17 23:01:49 +0300

    func_reviews

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,12 +1,19 @@
 from rest_framework import routers
 from django.urls import include, path
 
-from .views import CategoryViewSet, GenreViewSet, TitleViewSet
+from .views import (
+    CategoryViewSet, GenreViewSet, TitleViewSet, ReviewViewSet)
+
 
 v1_router = routers.DefaultRouter()
-v1_router.register('category', CategoryViewSet, basename='category')
-v1_router.register('genre', GenreViewSet, basename='genre')
-v1_router.register('title', TitleViewSet, basename='title')
+v1_router.register(r'categories', CategoryViewSet, basename='categories')
+v1_router.register(r'genres', GenreViewSet, basename='genres')
+v1_router.register(r'titles', TitleViewSet, basename='titles')
+v1_router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
 
 urlpatterns = [
     path('v1/', include(v1_router.urls)),
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,16 +1,21 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from django.shortcuts import get_object_or_404
+
 from rest_framework import filters, mixins, viewsets
 from rest_framework.viewsets import GenericViewSet
+
 from .serializers import (
     CategorySerializer, GenreSerializer, TitleSerializer,
-)
+    ReviewSerializer)
+
+from reviews.models import Title
 
 
 class GetPostViewSet(
     mixins.CreateModelMixin,
     mixins.ListModelMixin,
     mixins.DestroyModelMixin,
-    GenericViewSet
+    GenericViewSet,
 ):
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
@@ -27,4 +32,26 @@ class GenreViewSet(GetPostViewSet):
 class TitleViewSet(viewsets.ModelViewSet):
     serializer_class = TitleSerializer
     filter_backends = (DjangoFilterBackend,)
-    filterset_fields = ('category__slug', 'genre__slug', 'name', 'year',)
+    filterset_fields = (
+        'category__slug',
+        'genre__slug',
+        'name',
+        'year',
+    )
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+
+    serializer_class = ReviewSerializer
+
+    def get_title(self):
+        return get_object_or_404(
+            Title, id=self.kwargs.get('title_id'))
+
+    def get_queryset(self):
+        title = self.get_title()
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        title = self.get_title()
+        serializer.save(author=self.request.user, title=title)
