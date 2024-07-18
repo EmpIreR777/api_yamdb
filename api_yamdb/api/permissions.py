7a2339785b3bac758062e4d93668af7940f992af
Fedor Samofalov <smofalovfd@gmail.com>
2024-07-18 13:40:25 +0300

    Доделаны вью-сеты и пермишены для категорий и жанров.

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -10,9 +10,20 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
             request.method in permissions.SAFE_METHODS
             or request.user.is_authenticated
         )
+
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
             or obj.author == request.user
             or request.user.role == CustomUser.MODERATOR
             or request.user.role == CustomUser.ADMIN)
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+            and request.user.role == CustomUser.ADMIN
+        )
