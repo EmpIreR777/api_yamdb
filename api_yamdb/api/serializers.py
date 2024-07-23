074b4c7ecc9afbdd5734bb0d939f19d7674b31f3
Fedor Samofalov <smofalovfd@gmail.com>
2024-07-23 19:28:04 +0300

    Исправлены замечания по первому ревью.

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,8 +1,6 @@
 from django.forms import ValidationError
 from django.shortcuts import get_object_or_404
-
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
 
 from reviews.models import Category, Genre, Title, Review, Comment
 
@@ -21,15 +19,28 @@ class GenreSerializer(serializers.ModelSerializer):
         fields = ('name', 'slug')
 
 
+class TitleCreateUpdateSerializer(serializers.ModelSerializer):
+    genre = serializers.SlugRelatedField(
+        queryset=Genre.objects.all(),
+        slug_field='slug',
+        many=True
+    )
+    category = serializers.SlugRelatedField(
+        queryset=Category.objects.all(),
+        slug_field='slug'
+    )
+
+    class Meta:
+        model = Title
+        fields = ('id', 'name', 'year', 'description', 'genre', 'category')
+
+
 class TitleSerializer(serializers.ModelSerializer):
 
     genre = GenreSerializer(read_only=True, many=True)
     category = CategorySerializer(read_only=True)
 
-    def get_rating(self, obj):
-        return obj.get_rating()
-
-    rating = serializers.SerializerMethodField()
+    rating = serializers.FloatField()
 
     class Meta:
         model = Title
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,74 +1,55 @@
+from django.db.models import Avg
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 
 from .mixins import CreateListDeleteViewSet
+from .filters import TitleFilter
 from .permissions import IsAdminOrReadOnly, IsAuthorOrReadOnly
 from .serializers import (
-    CategorySerializer, GenreSerializer, TitleSerializer,
-    ReviewSerializer, CommentSerializer)
-from reviews.models import Category, Genre, Title, Review
-from .filters import TitleFilter
+    CategorySerializer, CommentSerializer, GenreSerializer,
+    ReviewSerializer, TitleSerializer, TitleCreateUpdateSerializer)
+from reviews.models import Category, Genre, Review, Title
 
 
 class CategoryViewSet(CreateListDeleteViewSet):
+    """Получение категории, добавление и удаление."""
+
     queryset = Category.objects.all()
     permission_classes = (IsAdminOrReadOnly,)
     serializer_class = CategorySerializer
 
-    def destroy(self, request, *args, **kwargs):
-        instance = get_object_or_404(Category, slug=self.kwargs['slug'])
-        self.perform_destroy(instance)
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
 
 class GenreViewSet(CreateListDeleteViewSet):
+    """Получение жанра, добавление и удаление."""
+
     queryset = Genre.objects.all()
     permission_classes = (IsAdminOrReadOnly,)
     serializer_class = GenreSerializer
 
-    def destroy(self, request, *args, **kwargs):
-        instance = get_object_or_404(Genre, slug=self.kwargs['slug'])
-        self.perform_destroy(instance)
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
 
 class TitleViewSet(viewsets.ModelViewSet):
-    queryset = Title.objects.all()
+    """Получение произведения, частичное обновление, добавление и удаление."""
+
+    queryset = Title.objects.all().select_related(
+        'category').prefetch_related('genre')
+    http_method_names = ['get', 'post', 'patch', 'delete']
     permission_classes = (IsAdminOrReadOnly,)
-    serializer_class = TitleSerializer
     filter_backends = (DjangoFilterBackend,)
     filterset_class = TitleFilter
 
-    def update(self, request, *args, **kwargs):
-        if not kwargs.get('partial'):
-            return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        return super().update(request, *args, **kwargs)
+    def get_serializer_class(self):
+        if self.action in ['create', 'update', 'partial_update']:
+            return TitleCreateUpdateSerializer
+        return TitleSerializer
 
-    def perform_create(self, serializer):
-        category = get_object_or_404(
-            Category,
-            slug=self.request.data['category'],
-        )
-        genres = [
-            get_object_or_404(
-                Genre,
-                slug=genre,
-            ) for genre in self.request.data.getlist('genre')
-        ]
-        serializer.save(category=category, genre=genres,)
-
-    def perform_update(self, serializer):
-        if 'category' in self.request.data:
-            serializer.validated_data['category'] = get_object_or_404(
-                Category,
-                slug=self.request.data['category'],
-            )
-        serializer.save()
+    def get_queryset(self):
+        return Title.objects.annotate(rating=Avg('reviews__score'))
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
+    """Получение обзора, частичное обновление, добавление и удаление."""
 
     serializer_class = ReviewSerializer
     permission_classes = (IsAuthorOrReadOnly,)
@@ -92,6 +73,7 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
 
 class CommentViewSet(viewsets.ModelViewSet):
+    """Получение комментариев, частичное обновление, добавление и удаление."""
 
     serializer_class = CommentSerializer
     permission_classes = (IsAuthorOrReadOnly,)
