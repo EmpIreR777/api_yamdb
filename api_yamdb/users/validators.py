9b8f9c6e503421f34301e163b63e9a5d6513af26
Alexander Kunin <K.u.n.i.n@yandex.ru>
2024-07-23 13:31:01 +0300

    Перемещение юзера.

--- api_yamdb/users/validators.py
+++
@@ -1,8 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def validate_not_me(value):
-    if value == 'me':
-        raise ValidationError(
-            'Использовать "me" в качестве имени пользователя запрещено.'
-        )
