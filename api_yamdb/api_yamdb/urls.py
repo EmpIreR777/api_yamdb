53319f57bc2530e48ab67548c89e64c7db19af63
Fedor Samofalov <smofalovfd@gmail.com>
2024-07-23 20:42:58 +0300

    Код приведен к PEP8.

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -4,7 +4,6 @@ from django.urls import include, path
 from django.views.generic import TemplateView
 
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
