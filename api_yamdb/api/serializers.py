bbb688e6a800ba65fd3bbeabe5c9d4d7e6f0a059
Fedor Samofalov <smofalovfd@gmail.com>
2024-07-25 16:34:45 +0300

    Доработан сериализатор произведений для post и patch запросов.

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.db.models import Avg
 from django.forms import ValidationError
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
